8e6f0e1f4b733e4ec41981b49c5264222b9c20f1
ika71 <ika71@naver.com>
2023-05-19 20:19:17 +0900

    로그인 구현(로컬스토리지 토큰 저장)

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+import SigninForm from "@/components/SigninForm";
+
+const Signin = () => {
+  return (
+    <main>
+      <SigninForm />
+    </main>
+  );
+};
+export default Signin;
---
+++ auth/LoginService.tsx
@@ -0,0 +1,50 @@
+import { backendUrl } from "@/url/backendUrl";
+
+interface LoginSuccess {
+  token: string;
+}
+/**
+ * 로그인 성공 시 로컬 스토리지 토큰 저장
+ * @param email
+ * @param password
+ * @returns 로그인 성공 true, 로그인 실패 false
+ */
+export const signin = async (email: string, password: string) => {
+  const member = {
+    email: email,
+    password: password,
+  };
+  const res = await authReqeust(`${backendUrl}/member/signin`, "post", member);
+  if (res.ok) {
+    const loginData: LoginSuccess = await res.json();
+    if (loginData.token) {
+      localStorage.setItem("token", loginData.token);
+      return true;
+    }
+  }
+
+  return false;
+};
+/**
+ * 헤더에 토큰을 담아서 url에 요청을 보냄
+ * @param url
+ * @param method
+ * @param json
+ * @returns fetch() 결과
+ */
+export const authReqeust = (url: string, method: string, json: object) => {
+  let headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8",
+  });
+
+  const accessToken = localStorage.getItem("token");
+  if (accessToken) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+  let http = {
+    headers: headers,
+    method: method,
+    body: JSON.stringify(json),
+  };
+  return fetch(url, http);
+};
