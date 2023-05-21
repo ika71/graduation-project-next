d1466186cacf8149fb196c29c141ff86f35ee8ea
ika71 <ika71@naver.com>
2023-05-21 15:03:32 +0900

    메인화면 임시 페이지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const HomePage = () => {
   return (
     <main>
-      <h1>Hello</h1>
+      <div className="relative">
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-blue-100">
+          <h2 className="text-4xl">임시 페이지</h2>
+          <p>공사중</p>
+        </div>
+      </div>
     </main>
   );
 };
