1551be4af8befe40e13d91672d4e2c22b7686f7c
ika71 <ika71@naver.com>
2023-07-14 17:40:39 +0900

    전자제품 자세히 보기 화면추가

---
+++ app/device/[deviceId]/page.tsx
@@ -0,0 +1,89 @@
+import { backendUrl } from "@/url/backendUrl";
+import Image from "next/image";
+
+interface FetchData {
+  id: number;
+  name: string;
+  categoryName: string;
+  imageId: number | null;
+  createdTime: string;
+  evalItemAvgDtoList: evalItemAvgDto[];
+}
+interface evalItemAvgDto {
+  id: number;
+  name: string;
+  avg: number | null;
+}
+
+const DeviceDetailPage = async ({
+  params,
+}: {
+  params: { deviceId: number };
+}) => {
+  const deviceId = params.deviceId;
+
+  const res = await fetch(`${backendUrl}/device/${deviceId}`, {
+    cache: "no-cache",
+  });
+  const deviceDetail: FetchData = await res.json();
+
+  const formatDate = (date: string) => {
+    const year = date.slice(0, 4);
+    const month = date.slice(5, 7);
+    const day = date.slice(8, 10);
+    return year + "-" + month + "-" + day;
+  };
+  return (
+    <div>
+      <h1>{deviceDetail.name}</h1>
+      <h1>{deviceDetail.categoryName}</h1>
+      <div>
+        {deviceDetail.imageId && (
+          <Image
+            src={`${backendUrl}/image/${deviceDetail.imageId}`}
+            alt={`${deviceDetail.imageId}`}
+            width={240}
+            height={240}
+            className="w-auto h-auto"
+          />
+        )}
+      </div>
+      <h1>{formatDate(deviceDetail.createdTime)}</h1>
+      <table className="min-w-full border-collapse block md:table">
+        <thead className="block md:table-header-group">
+          <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              평가항목 이름
+            </th>
+            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              평점
+            </th>
+          </tr>
+        </thead>
+        <tbody className="block md:table-row-group">
+          {deviceDetail.evalItemAvgDtoList.map((evalItemAvg) => (
+            <tr
+              key={evalItemAvg.id}
+              className="bg-gray-300 border border-grey-500 md:border-none block md:table-row"
+            >
+              <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                <span className="inline-block w-1/3 md:hidden font-bold">
+                  평가항목 이름
+                </span>
+                <h1>{evalItemAvg.name}</h1>
+              </td>
+              <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                <span className="inline-block w-1/3 md:hidden font-bold">
+                  평점
+                </span>
+                <h1>{evalItemAvg.avg || "아직 평점이 없습니다."}</h1>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DeviceDetailPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PaginationComponent from "@/components/PaginationComponent";
 import { backendUrl } from "@/url/backendUrl";
 import Image from "next/image";
+import Link from "next/link";
 
 interface FetchData {
   devicePagingDtoList: DevicePagingDto[];
@@ -32,8 +33,7 @@ const HomePage = async ({
     }
   );
   const fetchData: FetchData = await res.json();
-  const devicePagingDtoList: DevicePagingDto[] =
-    await fetchData.devicePagingDtoList;
+  const devicePagingDtoList: DevicePagingDto[] = fetchData.devicePagingDtoList;
 
   const totalCount = fetchData.totalCount;
 
@@ -138,55 +138,55 @@ const HomePage = async ({
               key={device.id}
               className="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 "
             >
-              <a href="#">
-                <div className="relative flex items-end overflow-hidden rounded-xl">
-                  {device.imageId && (
-                    <Image
-                      src={`${backendUrl}/image/${device.imageId}`}
-                      alt={`${device.imageId}`}
-                      width={240}
-                      height={240}
-                      className="w-auto h-auto"
-                    />
-                  )}
-                  <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 text-yellow-400"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <span className="ml-1 text-sm text-slate-400">4.9</span>
-                  </div>
+              <div className="relative flex items-end overflow-hidden rounded-xl">
+                {device.imageId && (
+                  <Image
+                    src={`${backendUrl}/image/${device.imageId}`}
+                    alt={`${device.imageId}`}
+                    width={240}
+                    height={240}
+                    className="w-auto h-auto"
+                  />
+                )}
+                <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 text-yellow-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                  <span className="ml-1 text-sm text-slate-400">4.9</span>
                 </div>
+              </div>
 
-                <div className="mt-1 p-2">
-                  <h2 className="text-slate-700">{device.name}</h2>
-                  {device.evaluationItemList.map((item) => (
-                    <span
-                      key={`${device.id + item}`}
-                      className="mt-1 text-sm text-slate-400"
-                    >
-                      {`${item} `}
-                    </span>
-                  ))}
+              <div className="mt-1 p-2">
+                <h2 className="text-slate-700">{device.name}</h2>
+                {device.evaluationItemList.map((item) => (
+                  <span
+                    key={`${device.id + item}`}
+                    className="mt-1 text-sm text-slate-400"
+                  >
+                    {`${item} `}
+                  </span>
+                ))}
 
-                  <div className="mt-3 flex items-end justify-between">
-                    <p className="text-lg font-bold text-blue-500">
-                      {device.categoryName}
-                    </p>
+                <div className="mt-3 flex items-end justify-between">
+                  <p className="text-lg font-bold text-blue-500">
+                    {device.categoryName}
+                  </p>
 
+                  <Link href={`/device/${device.id}`}>
                     <div className="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                       <button className="text-sm">자세히보기</button>
                     </div>
-                  </div>
-                  <p className="text-sm font-bold text-blue-500">
-                    등록일: {formatDate(device.createdTime)}
-                  </p>
+                  </Link>
                 </div>
-              </a>
+                <p className="text-sm font-bold text-blue-500">
+                  등록일: {formatDate(device.createdTime)}
+                </p>
+              </div>
             </article>
           ))}
         </div>
