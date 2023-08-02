2029f94590559639fff84bde8d0b518930fb82d7
ika71 <ika71@naver.com>
2023-08-02 17:15:04 +0900

    게시글 보기 화면 추가

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,78 @@
+import { backendUrl } from "@/url/backendUrl";
+
+interface BoardDetail {
+  id: number;
+  title: string;
+  content: string;
+  createdBy: string;
+  createdTime: string;
+}
+
+const BoardDetailPage = async ({ params }: { params: { boardId: number } }) => {
+  const boardId = params.boardId;
+  const res = await fetch(`${backendUrl}/board/${boardId}`, {
+    next: { revalidate: 1 },
+  });
+  const boardDetail: BoardDetail = await res.json();
+
+  const formatDate = (date: string) => {
+    const year = date.slice(0, 4);
+    const month = date.slice(5, 7);
+    const day = date.slice(8, 10);
+    const time = date.slice(11, 16);
+    return year + "-" + month + "-" + day + "-" + time;
+  };
+  return (
+    <>
+      <form>
+        <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
+          <div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+            <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">
+              {boardDetail.title}
+            </h1>
+            <div className="space-y-4">
+              <div>
+                <textarea
+                  id="content"
+                  cols={30}
+                  rows={10}
+                  value={boardDetail.content}
+                  className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"
+                ></textarea>
+              </div>
+              <div>
+                <label htmlFor="name" className="text-lx font-serif">
+                  Name:
+                </label>
+                <input
+                  value={boardDetail.createdBy}
+                  type="text"
+                  placeholder="name"
+                  id="name"
+                  className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="text-lx font-serif">
+                  작성일
+                </label>
+                <input
+                  value={formatDate(boardDetail.createdTime)}
+                  type="text"
+                  placeholder="name"
+                  id="email"
+                  className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+                />
+              </div>
+              <button className=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">
+                ADD POST
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default BoardDetailPage;
