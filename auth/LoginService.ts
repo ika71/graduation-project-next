80fb7f6005d5d0f0068f8c0da84fed401ecbd2c1
ika71 <ika71@naver.com>
2023-08-10 19:21:36 +0900

    LoginService deprecated 함수 삭제

--- auth/LoginService.ts
+++ auth/LoginService.ts
@@ -1,31 +1,3 @@
-import { backendUrl } from "@/url/backendUrl";
-
-interface LoginSuccess {
-  token: string;
-}
-/**
- * 로그인 성공 시 로컬 스토리지 토큰 저장
- * @deprecated userContext에 있는 함수로 변경
- * @param email
- * @param password
- * @returns 로그인 성공 true, 로그인 실패 false
- */
-export const signin = async (email: string, password: string) => {
-  const member = {
-    email: email,
-    password: password,
-  };
-  const res = await authReqeust(`${backendUrl}/member/signin`, "POST", member);
-  if (res.ok) {
-    const loginData: LoginSuccess = await res.json();
-    if (loginData.token) {
-      localStorage.setItem("token", loginData.token);
-      return true;
-    }
-  }
-
-  return false;
-};
 /**
  * 헤더에 토큰을 담아서 url에 요청을 보냄
  * @param url
@@ -71,13 +43,7 @@ export const authReqeustWithOutBody = (url: string, method: string) => {
   };
   return fetch(url, http);
 };
-/**
- * 로컬스토리지에서 토큰 제거
- * @deprecated userContext에 있는 함수로 변경
- */
-export const signout = () => {
-  localStorage.removeItem("token");
-};
+
 /**
  * 헤더에 토큰을 담아서 url에 요청을 보냄
  * @param url
@@ -102,16 +68,4 @@ export const authReqeustFile = (
     body: files,
   };
   return fetch(url, http);
-};
-
-/**
- * @deprecated userContext 상태 변수로 대체
- * @returns 로그인 토큰 존재 시 true 토큰 없으면 false
- */
-export const isLogin = () => {
-  if (localStorage.getItem("token")) {
-    return true;
-  } else {
-    return false;
-  }
 };
