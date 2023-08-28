c047979299800907dd85a23af47bd5dd792c675f
ika71 <ika71@naver.com>
2023-08-28 18:06:39 +0900

    비로그인 상태일 때 게시글 수정 삭제 버튼 숨김

--- components/BoardDetailClient.tsx
+++ components/BoardDetailClient.tsx
@@ -36,20 +36,28 @@ const BoardDetailClient = ({ boardId, children }: PropsWithChildren<Props>) => {
     }
   };
 
+  if (!userContext) {
+    return <></>;
+  }
+
   return (
     <>
       {children}
-      <Link href={`/board/${boardId}/modify`}>
-        <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded mr-3">
-          수정
-        </button>
-      </Link>
-      <button
-        onClick={deleteBoard}
-        className="my-5 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded mr-3"
-      >
-        삭제
-      </button>
+      {userContext.isLogin && (
+        <div>
+          <Link href={`/board/${boardId}/modify`}>
+            <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded mr-3">
+              수정
+            </button>
+          </Link>
+          <button
+            onClick={deleteBoard}
+            className="my-5 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded mr-3"
+          >
+            삭제
+          </button>
+        </div>
+      )}
     </>
   );
 };
