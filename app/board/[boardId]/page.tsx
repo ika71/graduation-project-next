ba43b65afdb119c99b81815bc9de15dcc0e7c40c
ika71 <ika71@naver.com>
2023-08-29 19:30:28 +0900

    boardDetail 컴포넌트 Client로 통합

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,8 +1,8 @@
 import BoardCommentClient from "@/components/BoardCommentClient";
 import BoardDetailClient from "@/components/BoardDetailClient";
-import BoardDetailServer from "@/components/BoardDetailServer";
+import { backendUrl } from "@/url/backendUrl";
 
-const BoardDetailPage = ({
+const BoardDetailPage = async ({
   params,
   searchParams,
 }: {
@@ -12,12 +12,15 @@ const BoardDetailPage = ({
   const boardId = params.boardId;
   const currentPage = searchParams.commentPage || 1;
 
+  const res = await fetch(`${backendUrl}/board/${boardId}`, {
+    cache: "no-store",
+  });
+  const boardDetail = await res.json();
+
   return (
     <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
       <div className="bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
-        <BoardDetailClient boardId={boardId}>
-          <BoardDetailServer boardId={boardId} />
-        </BoardDetailClient>
+        <BoardDetailClient boardDetail={boardDetail} />
         <BoardCommentClient boardId={boardId} currentPage={currentPage} />
       </div>
     </div>
--- components/BoardDetailClient.tsx
+++ components/BoardDetailClient.tsx
@@ -4,16 +4,28 @@ import UserContext from "@/context/userContext";
 import { backendUrl } from "@/url/backendUrl";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { PropsWithChildren, useContext } from "react";
+import { useContext } from "react";
 
 interface Props {
-  boardId: number;
+  boardDetail: BoardDetail;
 }
+
+interface BoardDetail {
+  id: number;
+  title: string;
+  content: string;
+  createdBy: string;
+  createdTime: string;
+}
+
 /**
- * @param boardId: 읽을 게시글의 id
+ * @param boardDetail 게시글 정보
  * @returns
  */
-const BoardDetailClient = ({ boardId, children }: PropsWithChildren<Props>) => {
+const BoardDetailClient = (props: Props) => {
+  const { boardDetail } = props;
+  const boardId = boardDetail.id;
+
   const userContext = useContext(UserContext);
   const router = useRouter();
 
@@ -42,22 +54,63 @@ const BoardDetailClient = ({ boardId, children }: PropsWithChildren<Props>) => {
 
   return (
     <>
-      {children}
-      {userContext.isLogin && (
+      <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">
+        {boardDetail.title}
+      </h1>
+      <div className="space-y-4">
         <div>
-          <Link href={`/board/${boardId}/modify`}>
-            <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded mr-3">
-              수정
-            </button>
-          </Link>
-          <button
-            onClick={deleteBoard}
-            className="my-5 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded mr-3"
-          >
-            삭제
-          </button>
+          <textarea
+            id="content"
+            cols={30}
+            rows={10}
+            defaultValue={boardDetail.content}
+            readOnly={true}
+            className="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"
+          ></textarea>
         </div>
-      )}
+        <div>
+          <label htmlFor="name" className="text-lx font-serif">
+            작성자
+          </label>
+          <input
+            defaultValue={boardDetail.createdBy}
+            readOnly={true}
+            type="text"
+            placeholder="name"
+            id="name"
+            className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-lx font-serif">
+            작성일
+          </label>
+          <input
+            defaultValue={boardDetail.createdTime}
+            readOnly={true}
+            type="text"
+            placeholder="name"
+            id="email"
+            className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+          />
+        </div>
+      </div>
+      {userContext.isLogin &&
+        userContext.userName === boardDetail.createdBy && (
+          <div>
+            <Link href={`/board/${boardId}/modify`}>
+              <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded mr-3">
+                수정
+              </button>
+            </Link>
+            <button
+              onClick={deleteBoard}
+              className="my-5 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded mr-3"
+            >
+              삭제
+            </button>
+          </div>
+        )}
     </>
   );
 };
