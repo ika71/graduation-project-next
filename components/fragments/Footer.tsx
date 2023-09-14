44dadd6fe80748018460820721b9114f2ecd8abc
ika71 <ika71@naver.com>
2023-09-14 20:58:35 +0900

    Footer css 변경

--- components/fragments/Footer.tsx
+++ components/fragments/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="text-center left-0 z-20 w-full p-4 border-t shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800 border-gray-600">
+      <span className="text-sm sm:text-center text-gray-400">
         © 2023 created by ika71
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
+        <li className="mx-auto">
           <a
             href="https://github.com/ika71"
             target="_blank"
