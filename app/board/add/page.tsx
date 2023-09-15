eff378abda9e8f5323cf0537e05df7e51aad8afe
ika71 <ika71@naver.com>
2023-09-15 16:47:22 +0900

    게시글 작성에 이미지 첨부 기능(모달) 추가

--- app/board/add/page.tsx
+++ app/board/add/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import BoardImageModal from "@/components/modal/board/BoardImageModal";
 import UserContext from "@/context/userContext";
 import { backendUrl } from "@/url/backendUrl";
 import { useRouter } from "next/navigation";
-import { useContext, useRef } from "react";
+import { FormEvent, useContext, useRef, useState } from "react";
+
+interface Image {
+  imageId: number;
+  originName: string;
+}
 
 const BoardAddPage = ({
   searchParams,
@@ -13,6 +19,9 @@ const BoardAddPage = ({
   const deviceId = searchParams.deviceId;
   const router = useRouter();
   const userContext = useContext(UserContext);
+
+  const [showUploadModal, setShowUploadModal] = useState(false);
+  const [uploadImages, setUploadImages] = useState<Image[]>([]);
 
   const titleRef = useRef<HTMLInputElement>(null);
   const contentRef = useRef<HTMLTextAreaElement>(null);
@@ -31,10 +40,23 @@ const BoardAddPage = ({
     if (!titleRef.current || !contentRef.current) {
       return;
     }
+    if (!titleRef.current.value.trim()) {
+      alert("제목은 비어 있을 수 없습니다.");
+      return;
+    }
+    if (!contentRef.current.value.trim()) {
+      alert("본문은 비어 있을 수 없습니다.");
+      return;
+    }
 
-    let board = {
+    let imageIdList: number[] = [];
+    uploadImages.forEach((uploadImage) =>
+      imageIdList.push(uploadImage.imageId)
+    );
+    const board = {
       title: titleRef.current.value,
       content: contentRef.current.value,
+      imageIdList: imageIdList,
     };
 
     const res = await userContext.authRequest(
@@ -50,31 +72,68 @@ const BoardAddPage = ({
     }
   };
 
+  const openUploadModal = (event: FormEvent) => {
+    event.preventDefault();
+    setShowUploadModal(true);
+  };
+  const closeUploadModal = () => {
+    setShowUploadModal(false);
+  };
+  const afterUpload = (uploadImages: Image[]) => {
+    setUploadImages(uploadImages);
+  };
+
   return (
-    <form className="py-10 md:px-40 bg-gray-100">
-      <div className="flex flex-col gap-y-5 border border-gray-500 min-h-screen py-10 px-4 md:px-20 shadow bg-white">
-        <input
-          ref={titleRef}
-          type="text"
-          placeholder="제목을 입력하세요"
-          className="border border-gray-500 pl-4 py-2 w-2/3 bg-blue-50"
-        ></input>
-        <textarea
-          ref={contentRef}
-          rows={20}
-          placeholder="내용을 입력하세요"
-          className="border border-gray-500 pl-4 pt-1 bg-blue-50"
-        ></textarea>
-        <div className="text-right">
-          <button
-            onClick={createBoard}
-            className="w-full md:w-fit md:mx-2 md:my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded"
-          >
-            글 작성
-          </button>
+    <>
+      {showUploadModal && (
+        <BoardImageModal
+          closeModal={closeUploadModal}
+          afterUpload={afterUpload}
+        />
+      )}
+      <form className="py-10 md:px-40 bg-gray-100">
+        <div className="flex flex-col gap-y-5 border border-gray-500 min-h-screen py-10 px-4 md:px-20 shadow bg-white">
+          <div>
+            <input
+              ref={titleRef}
+              type="text"
+              placeholder="제목을 입력하세요"
+              className="border border-gray-500 pl-4 py-2 w-2/3 bg-blue-50 mr-5 mb-5"
+            ></input>
+            <button
+              onClick={openUploadModal}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded"
+            >
+              이미지 업로드
+            </button>
+          </div>
+          <textarea
+            ref={contentRef}
+            rows={20}
+            placeholder="내용을 입력하세요"
+            className="border border-gray-500 pl-4 pt-1 bg-blue-50"
+          ></textarea>
+          <div>
+            첨부파일:
+            {!uploadImages && <span> 없음</span>}
+            {uploadImages &&
+              uploadImages.map((uploadImage) => {
+                return (
+                  <p key={uploadImage.imageId}>{uploadImage.originName}</p>
+                );
+              })}
+          </div>
+          <div className="text-right">
+            <button
+              onClick={createBoard}
+              className="w-full md:w-fit md:mx-2 md:my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded"
+            >
+              글 작성
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 export default BoardAddPage;
---
+++ components/modal/board/BoardImageModal.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import UserContext from "@/context/userContext";
+import { backendUrl } from "@/url/backendUrl";
+import { ChangeEvent, useContext, useState } from "react";
+
+interface Props {
+  closeModal: () => void;
+  afterUpload: (uploadImages: Image[]) => void;
+}
+
+interface UploadSuccess {
+  imageList: Image[];
+}
+
+interface Image {
+  imageId: number;
+  originName: string;
+}
+
+/**
+ * 게시판 이미지 업로드 모달
+ * @param closeModal: () => void 모달창을 닫을 때 실행할 함수
+ * @param afterUpload: (uploadImages: Image[]) => void 업로드 이후에 실행할 함수
+ * 업로드한 이미지의 정보를 매개변수로 넘긴다.
+ */
+const BoardImageModal = (props: Props) => {
+  const { closeModal, afterUpload } = props;
+  const [uploadFiles, setUploadFiles] = useState<File[]>([]);
+  const userContext = useContext(UserContext);
+  if (!userContext || userContext.userName === "") return <></>;
+
+  const uploadFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    if (event.target.files.length > 5) {
+      alert("이미지는 최대 5장까지 업로드 할 수 있습니다.");
+      return;
+    }
+    const addFiles = Array.from(event.target.files);
+    setUploadFiles((prev) => prev.concat(addFiles));
+  };
+
+  const uploadFileRequest = async () => {
+    if (uploadFiles.length === 0) return;
+    const formData = new FormData();
+    uploadFiles.forEach((uploadFile) =>
+      formData.append("imageFile", uploadFile)
+    );
+
+    const res = await userContext.authRequest(
+      `${backendUrl}/image`,
+      "POST",
+      formData
+    );
+    if (res.ok) {
+      const uploadSuccess: UploadSuccess = await res.json();
+      const uploadImages = uploadSuccess.imageList;
+      afterUpload(uploadImages);
+      closeModal();
+    } else {
+      alert(await res.text());
+    }
+  };
+
+  return (
+    <div className="fixed z-30 inset-0 flex justify-center items-center overflow-y-auto">
+      <div
+        id="backdrop"
+        onClick={closeModal}
+        className="fixed h-screen w-full left-0 top-0 bg-gray-500 opacity-75"
+      ></div>
+
+      <div className="absolute top-auto bg-white rounded shadow-lg w-10/12 md:w-1/3 transform">
+        <div className="border-b px-4 py-2">
+          <h3 className="font-semibold text-lg">이미지 업로드</h3>
+        </div>
+
+        <div className="p-3 text-center">
+          <input
+            id="imageUpload"
+            type="file"
+            multiple
+            onChange={uploadFileChange}
+            accept="image/*"
+            className="hidden"
+          />
+          <label
+            htmlFor="imageUpload"
+            className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white"
+          >
+            파일선택
+          </label>
+        </div>
+
+        <div className="p-3 text-center">
+          {uploadFiles.map((uploadFile, index) => {
+            return (
+              <p key={index}>
+                {uploadFile.name}{" "}
+                <svg
+                  onClick={() => {
+                    setUploadFiles((prev) =>
+                      prev.filter((filterFile) => filterFile !== uploadFile)
+                    );
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 cursor-pointer inline-block"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </p>
+            );
+          })}
+        </div>
+
+        <div className="p-3 text-center"></div>
+        <div className="flex justify-end gap-x-5 items-center w-100 border-t p-3">
+          <button
+            onClick={uploadFileRequest}
+            className="bg-blue-600 hover:bg-blue-700 px-3 py-1 rounded text-white"
+          >
+            업로드
+          </button>
+          <button
+            onClick={closeModal}
+            className="bg-red-600 hover:bg-red-700 px-3 py-1 rounded text-white mr-1 close-modal"
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardImageModal;
