9166af29df319c81a21cedd63d0b41b34e7a44a7
ika71 <ika71@naver.com>
2023-09-16 20:57:00 +0900

    어드민 메뉴 화면 css, router 추가, 카테고리 추가 모달 백드롭 클릭시 닫기 추가

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,21 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const AdminMainPage = () => {
+  const router = useRouter();
+
   return (
     <section className="text-gray-600 body-font bg-gray-50 h-screen flex justify-center items-center ">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4 text-center justify-center items-center">
-          <div className="p-4 sm:w-1/2 lg:w-1/3 w-full hover:scale-105 duration-500">
+          <div
+            onClick={() => router.push("/admin/category")}
+            className="cursor-pointer p-4 sm:w-1/2 lg:w-1/3 w-full hover:scale-105 duration-500"
+          >
             <div className=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md">
               <div>
                 <h2 className="text-gray-900 text-lg font-bold">
                   카테고리 관리
                 </h2>
-                <Link href={"/admin/category"}>
-                  <button className="text-sm mt-6 px-4 py-2 bg-yellow-400 text-white rounded-lg  tracking-wider hover:bg-yellow-300 outline-none">
-                    이동
-                  </button>
-                </Link>
               </div>
               <div className="bg-gradient-to-tr from-yellow-500 to-yellow-400 w-32 h-32  rounded-full shadow-2xl shadow-yellow-400 border-white  border-dashed border-2  flex justify-center items-center ">
                 <div>
@@ -24,17 +27,15 @@ const AdminMainPage = () => {
               </div>
             </div>
           </div>
-          <div className="p-4 sm:w-1/2 lg:w-1/3 w-full hover:scale-105 duration-500">
+          <div
+            onClick={() => router.push("/admin/device")}
+            className="cursor-pointer p-4 sm:w-1/2 lg:w-1/3 w-full hover:scale-105 duration-500"
+          >
             <div className=" flex items-center  justify-between p-4  rounded-lg bg-white shadow-indigo-50 shadow-md">
               <div>
                 <h2 className="text-gray-900 text-lg font-bold">
                   전자제품 관리
                 </h2>
-                <Link href={"/admin/device"}>
-                  <button className="text-sm mt-6 px-4 py-2 bg-orange-400  text-white rounded-lg  tracking-wider hover:bg-orange-500 outline-none">
-                    이동
-                  </button>
-                </Link>
               </div>
               <div className="bg-gradient-to-tr from-orange-500 to-orange-400 w-32 h-32  rounded-full shadow-2xl shadow-orange-400 border-white  border-dashed border-2  flex justify-center items-center ">
                 <div>
--- components/modal/category/CategoryAddModal.tsx
+++ components/modal/category/CategoryAddModal.tsx
@@ -45,7 +45,10 @@ const CategoryAddModal = (props: Props) => {
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          <div
+            onClick={closeModal}
+            className="absolute inset-0 bg-gray-500 opacity-75"
+          ></div>
         </div>
 
         <span
