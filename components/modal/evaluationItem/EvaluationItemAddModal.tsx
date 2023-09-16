8d75ef074de2ef8f0593b08af2ab126d417c59e8
ika71 <ika71@naver.com>
2023-09-16 21:57:16 +0900

    어드민 평가항목 관리 modal 수정

--- components/modal/evaluationItem/EvaluationItemAddModal.tsx
+++ components/modal/evaluationItem/EvaluationItemAddModal.tsx
@@ -23,6 +23,10 @@ const EvaluationItemAddModal = (props: Props) => {
     if (!evaluationItemName.current || !userContext) {
       return;
     }
+    if (evaluationItemName.current.value.trim().length === 0) {
+      alert("이름을 입력하세요");
+      return;
+    }
     const evaluationItem = {
       electronicDeviceId: deviceId,
       name: evaluationItemName.current.value,
@@ -43,7 +47,10 @@ const EvaluationItemAddModal = (props: Props) => {
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          <div
+            onClick={closeModal}
+            className="absolute inset-0 bg-gray-500 opacity-75"
+          ></div>
         </div>
 
         <span
@@ -56,14 +63,15 @@ const EvaluationItemAddModal = (props: Props) => {
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
-              <div className="mt-3 text-center sm:mt-0 sm:text-left">
+              <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
                 <label className="font-semibold text-sm text-gray-600 pb-1 block">
                   평가항목 이름
                 </label>
                 <input
                   type="text"
+                  placeholder="이름을 입력하세요"
                   ref={evaluationItemName}
-                  className="border rounded-lg pr-64 px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  className="border-2 border-blue-300 rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
                 />
                 <button
                   type="button"
