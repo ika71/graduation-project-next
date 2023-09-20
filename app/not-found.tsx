fd4ae2f536c027cb1c4dd7b43db5799b1557b666
ika71 <ika71@naver.com>
2023-09-20 15:41:54 +0900

    404 페이지 css 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link href="/">Return Home</Link>
+    <div className="h-screen flex justify-center">
+      <div className="mt-8">
+        <h1 className="font-bold text-5xl my-4">404 Not Found</h1>
+        <Link href="/" className="text-xl text-blue-600 underline">
+          Return Home
+        </Link>
+      </div>
     </div>
   );
 }
