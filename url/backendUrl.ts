9e35d368752d4d72adcde596b1baeb138587a7b6
ika71 <ika71@naver.com>
2023-09-24 15:48:24 +0900

    환경변수 이름 변경

--- url/backendUrl.ts
+++ url/backendUrl.ts
@@ -1,2 +1,3 @@
 export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_SERVER;
-export const serverFetchBackendUrl = process.env.SERVER_FETCH_BACKEND_SERVER;
+export const serverFetchBackendUrl =
+  process.env.NEXT_PUBLIC_SERVER_FETCH_BACKEND_SERVER;
