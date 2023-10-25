e3ca41178edbe0b6e60b3990a9ab76f9ffb08d99
ika71 <ika71@naver.com>
2023-10-25 13:36:18 +0900

    메인페이지 전자제품 총 평점 표시

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ interface Device {
   imageId: string | null;
   createdTime: string;
   evaluationItemList: string[];
+  totalAvg: number;
 }
 
 const HomePage = async ({
@@ -59,15 +60,31 @@ const HomePage = async ({
             <Link href={`device/${device.id}`} className="cursor-pointer">
               <div className="my-8 rounded shadow-lg shadow-gray-200 bg-white hover:scale-105 duration-500">
                 <div>
-                  {device.imageId && (
-                    <Image
-                      src={`${backendUrl}/image/${device.imageId}`}
-                      alt={device.imageId}
-                      width={300}
-                      height={300}
-                      className="rounded-t h-72 w-full"
-                    />
-                  )}
+                  <div className="relative flex items-end overflow-hidden rounded-xl">
+                    {device.imageId && (
+                      <Image
+                        src={`${backendUrl}/image/${device.imageId}`}
+                        alt={device.imageId}
+                        width={300}
+                        height={300}
+                        className="rounded-t h-72 w-full"
+                      />
+                    )}
+                    <div className="absolute bottom-3 left-3 inline-flex items-center rounded-lg bg-white p-2 shadow-md">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-yellow-400"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                      </svg>
+                      <span className="ml-1 text-sm text-slate-400">
+                        {device.totalAvg || "평점 없음"}
+                      </span>
+                    </div>
+                  </div>
+
                   <div className="p-4">
                     <p className="text-lg mb-4 font-bold leading-relaxed text-gray-800">
                       {device.name}
