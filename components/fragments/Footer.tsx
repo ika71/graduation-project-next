161c3058451738cedbcfa8edf921ab226c9c6295
ika71 <ika71@naver.com>
2023-11-05 13:11:59 +0900

    Footer 수정

--- components/fragments/Footer.tsx
+++ components/fragments/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="text-center left-0 z-20 w-full p-4 border-t shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800 border-gray-600">
       <span className="text-sm sm:text-center text-gray-400">
-        © 2023 created by ika71
+        2023 created by ika71
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
         <li className="mx-auto">
